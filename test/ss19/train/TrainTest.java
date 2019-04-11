aa73c2e9be56d153d1e5a704aefea0ab343e643c
Eduard.Bartolovic <Eduard.Bartolovic@posteo.de>
2019-04-11 13:52:53 +0200

    train

---
+++ test/ss19/train/TrainTest.java
@@ -0,0 +1,80 @@
+package ss19.train;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import ss19.train.Train;
+import ss19.train.waggon.Type;
+import ss19.train.waggon.Waggon;
+
+/**
+ *
+ * @author Eduard Li530S
+ */
+public class TrainTest {
+    
+    public TrainTest() {
+    }
+
+    /**
+     * Test of enlarge method, of class Train.
+     */
+    @Test(timeout = 1000)
+    public void testEnlarge() {
+        Train instance = new Train();
+        assertEquals(1, instance.getCapacity());
+        instance.enlarge(Type.ENGINE);
+        assertEquals(2, instance.getCapacity());
+        instance.enlarge(Type.BISTRO);
+        assertEquals(12, instance.getCapacity());
+        instance.enlarge(Type.FIRST);
+        assertEquals(32, instance.getCapacity());
+        instance.enlarge(Type.SECOND);
+        assertEquals(62, instance.getCapacity());
+    }
+
+
+    /**
+     * Test of shorten and enlarge method, of class Train.
+     */
+    @Test(timeout = 1000)
+    public void testIntegrety() {
+        Train instance = new Train();
+        assertEquals(1, instance.getCapacity());
+        Waggon waggon = instance.getEngine();
+        assertEquals(Type.ENGINE, waggon.getType());
+        
+        instance.enlarge(Type.BISTRO);
+        
+        waggon = instance.getEngine();
+        assertEquals(Type.ENGINE, waggon.getType());
+        assertEquals(Type.BISTRO, waggon.getNext().getType());
+        
+        instance.enlarge(Type.FIRST);
+        
+        assertEquals(Type.FIRST, waggon.getNext().getNext().getType());
+        
+        instance.enlarge(Type.FIRST);
+        assertEquals(Type.FIRST, waggon.getNext().getNext().getNext().getType());
+        
+        instance.enlarge(Type.BISTRO);
+        
+        assertEquals(Type.BISTRO, waggon.getNext().getNext().getNext().getNext().getType());
+        
+        instance.enlarge(Type.SECOND);
+        
+        assertEquals(Type.SECOND, waggon.getNext().getNext().getNext().getNext().getNext().getType());
+        assertEquals(null, waggon.getNext().getNext().getNext().getNext().getNext().getNext());
+    }
+    
+
+    /**
+     * Test of getEngine method, of class Train.
+     */
+    @Test(timeout = 1000)
+    public void testGetEngine() {
+        Train instance = new Train();
+        Waggon result = instance.getEngine();
+        assertEquals(Type.ENGINE, result.getType());
+    }
+
+}
